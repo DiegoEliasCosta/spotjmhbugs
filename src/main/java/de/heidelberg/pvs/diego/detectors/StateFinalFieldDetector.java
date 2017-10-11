17c32aaa3990cb9f12316c4e3b72f44d0ea3b182
DiegoCosta <diegoelias1@gmail.com>
2017-10-11 14:53:22 +0200

    Adding the source line on the final state report.

--- src/main/java/de/heidelberg/pvs/diego/detectors/StateFinalFieldDetector.java
+++ src/main/java/de/heidelberg/pvs/diego/detectors/StateFinalFieldDetector.java
@@ -22,9 +22,9 @@ public class StateFinalFieldDetector extends AbstractJMHStateClassDetector {
 			// RULE: Fields on @State object should not be declared final
 			if (obj.isFinal() && !obj.isSynthetic()) {
 				
-				
 				BugInstance bugInstance = new BugInstance(this, JMH_STATE_FINAL_FIELD, NORMAL_PRIORITY).addField(this)
-						.addClass(this);
+						.addClass(this)
+						.addSourceLine(this);
 
 				super.bugReporter.reportBug(bugInstance);
 			}
